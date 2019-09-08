7f420d921f1f40a5d5db26a3da9ada5bdc6dfd00
Gabri van Lee <gvanlee@gmail.com>
2019-09-08 17:04:00 +0200

    Update for SDK 2

--- app.js
+++ app.js
@@ -1,99 +1,117 @@
+'use strict';
 
+const Homey = require('homey');
 const http = require('http');
-const ledring = Homey.manager('ledring');
-const cron = Homey.manager('cron');
-const settings = Homey.manager('settings');
-const flow = Homey.manager('flow');
 
-function init() {
-	myLog(__('starting'));
+class MyApp extends Homey.App {
+	
+	onInit() {
+		this.currentStatus = 'F';
+		this.logMessage('nl.deskweb.network-check is being initialized');
 
-    settings.set('curstatus', 'F');
-    
-    cron.registerTask('deskweb-netcheck', '* * * * *', null, function(err, success) {
-        if (err) {
-            // usually this means the task already exists. not a problem
-            myLog(err);
+        // Initiele test om de boel aan te zwengelen
+        this.verifyConnection();
+	}
+
+    logMessage(sLog) {
+        // Only for debug purposes
+        if (false) {
+            this.log(sLog);
         }
-    });
-    
-    cron.on('deskweb-netcheck', function(data) {
-        myLog(__('verifying'));
-        verifyConnection();
-    });
-
-    // crontab verwijderen
-    Homey.on('unload', function(){
-        cron.unregisterTask('deskweb-netcheck', function(err, success) {
-            myLog(__('unloading'));
-        });
-    });
-}
-
-function verifyConnection() {
-    if (settings.get('curstatus') == 'X') {
-        // testmode, just say it stopped working
-        myLog(__('testmode'));
-        notifyUser(true);
     }
-    else {
-        // 74.125.133.94 = google.nl
-        var chunk, data;
+
+    verifyConnection() {
+		// 74.125.133.94 = google.nl
+		var chunk, data;
+		
+		var req = http.request({
+				host:   '1.1.1.1',
+				path:   '/',
+				method: 'GET'
+			},
+			(response) => {
+				response.on('data', chunk => data += chunk);
+				response.on('end', () => this.notifyUser(false));
+			});
+
+		req.on('error', e => this.notifyUser(true));
+		req.end();
+
+        var myApp = this;
+        setTimeout(function () { 
+            myApp.verifyConnection(); 
+        }, 15000);
+	}
+		
+    notifyUser(PbProblem) {
+        var LaColor = (! PbProblem ?{ r: 0, g: 160, b: 0 } : { r: 160, g: 0, b: 0 } );
+        var LnLength = (PbProblem ? 2500 : 125);
+        var LsTrigger = (PbProblem ? 'connection_lost' : 'connection_restored');
+        var LsMessage;
+    
+        var LbNewstatus = (PbProblem ? 'T' : 'F');
         
-        var req = http.request({
-                host:   '74.125.133.94',
-                path:   '/',
-                method: 'GET'
-            },
-            (response) => {
-                response.on('data', chunk => data += chunk);
-                response.on('end', () => notifyUser(false));
-            });
+        var LaFrames = [];
+        var LaFrame = [];
 
-        req.on('error', e => notifyUser(true));
-        req.end();
-    }
-}
+        for (var i = 0; i < 24; i++) {
+            LaFrame.push(LaColor);
+        }
+        LaFrames.push(LaFrame);
 
-function myLog(PsMessage) {
-    Homey.log(PsMessage);
-}
+        try {
+			this.logMessage('animating: status = ' +  LbNewstatus );
 
-function notifyUser(PbProblem) {
-    var LsColor = (! PbProblem ? '#00f000' : '#a00000');
-    var LnLength = (PbProblem ? 2500 : 125);
-    var LsTrigger = (PbProblem ? 'connection_lost' : 'connection_restored');
-    var LsMessage;
-
-    var LbNewstatus = (PbProblem ? 'T' : 'F');
+            const myAnimation = new Homey.LedringAnimation({
+                options: {
+                  fps: 1,       // real frames per second
+                  tfps: 60,     // target frames per second. this means that every frame will be interpolated 60 times
+                  rpm: 0,       // rotations per minute
+                },
+                frames: LaFrames,
+                priority: 'INFORMATIVE', // or FEEDBACK, or CRITICAL
+                duration: LnLength, // duration in ms, or keep empty for infinite
+              });
+              
+              // register the animation with Homey
+              myAnimation
+                .on('start', () => {
+                  // The animation has started playing
+                })
+                .on('stop', () => {
+                    // The animation has stopped playing
+                })
+                .register()
+                  .then( () => {
+                    this.log('Animation registered!');
+              
+                    myAnimation.start();
+                  })
+                  .catch( this.error );            
+        }
+        catch (err) {
+            this.logMessage('exception: err = ' + err);
+        }
     
-    try {
-        if (PbProblem || settings.get('hbeat') == '1') {
-            myLog(__('animating', { 'status': LbNewstatus }));
-            
-            ledring.animate('pulse', { color: LsColor, rpm: 4 }, 'INFORMATIVE', LnLength, function(err, success) {
-                if (err) {
-                    myLog(err);
-                }
-            });
-        }
-    }
-    catch (err) {
-        myLog(__('exception', { 'err': err }));
-    }
+        try {
+            if (this.currentStatus != LbNewstatus) {
+                this.currentStatus = LbNewstatus;
+    
+                // flow triggeren als we een melding binnen hebben gekregen
+                this.logMessage('triggering: type = ' + LsTrigger);
 
-    try {
-        if (settings.get('curstatus') != LbNewstatus) {
-            settings.set('curstatus', LbNewstatus);
-
-            // flow triggeren als we een melding binnen hebben gekregen
-            myLog(__('triggering', { 'type': LsTrigger }));
-            flow.trigger(LsTrigger, { });
+				let rainStartTrigger = new Homey.FlowCardTrigger(LsTrigger);
+				rainStartTrigger
+					.register()
+					.trigger()
+						.catch( this.error )
+						.then( this.log('flow triggered succesfully') )
+            }
         }
-    }
-    catch (err) {
-        myLog(__('exception', { 'err': err }));
-    }
+        catch (err) {
+            this.logMessage('exception: err = ' + err);
+        }
+    }	
 }
 
-module.exports.init = init;
+module.exports = MyApp;
